fa824c62ce3ad181861d24d2020bcf6edd75779c
Tim Weber <scy@scytale.name>
2012-10-31 12:21:38 +0100

    Support fswebcam for capturing
    Also moves the file name into a separate variable.

--- autosnap.sh
+++ autosnap.sh
@@ -13,12 +13,16 @@ die() {
 	exit "$rc"
 }
 
-if ! which imagesnap >/dev/null 2>&1; then
-	die 2 "cannot find imagesnap in $PATH"
-fi
-
 if [ ! -d "$outdir" ]; then
 	die 3 "outdir is not a directory: $outdir"
 fi
 
-imagesnap "$outdir/$(date '+%Y-%m-%d_%H-%M-%S')_autosnap_$(hostname -s).jpg"
+outfile="$outdir/$(date '+%Y-%m-%d_%H-%M-%S')_autosnap_$(hostname -s).jpg"
+
+if which imagesnap >/dev/null 2>&1; then
+	imagesnap "$outfile"
+elif which fswebcam >/dev/null 2>&1; then
+	fswebcam --delay 2 --frames 1 --jpeg 90 --no-banner "$outfile"
+else
+	die 2 "cannot find imagesnap or fswebcam in $PATH"
+fi
