3f932ccbc31c178593959368fc7ea28f799634f1
Tim Weber <scy@scytale.name>
2013-01-23 23:25:07 +0100

    Accept even more input file name patterns

--- autosnap-renamer.sh
+++ autosnap-renamer.sh
@@ -5,7 +5,7 @@ cd "$AUTOSNAPRNIN"
 
 for from in *; do
 	# Build a "to" name by fixing the date format and position in file names.
-	to="$(echo "$from" | sed -e 's/^autosnap[_-]\([^_-]*\)\.\(..\)\.\(..\)\.\(....\)\.\(..\)\.\(..\)\.\(..\)\.jpg$/\4-\3-\2_\5-\6-\7_autosnap_\1.jpg/' -e 's/^autosnap[_-]\([^_-]*\)\.\(....\)\.\(..\)\.\(..\)\.\(..\)\.\(..\)\.\(..\)\.jpg$/\2-\3-\4_\5-\6-\7_autosnap_\1.jpg/')"
+	to="$(echo "$from" | sed -e 's/^autosnap[_-]\([^_-]*\)\.\(..\)[.-]\(..\)[.-]\(....\)\.\(..\)\.\(..\)\.\(..\)\.jpg$/\4-\3-\2_\5-\6-\7_autosnap_\1.jpg/' -e 's/^autosnap[_-]\([^_-]*\)\.\(....\)[.-]\(..\)[.-]\(..\)\.\(..\)\.\(..\)\.\(..\)\.jpg$/\2-\3-\4_\5-\6-\7_autosnap_\1.jpg/')"
 	# If the file name has not changed, there's no sense in renaming it.
 	if [ "$from" != "$to" ]; then
 		mv "$from" "$to"
