af545f5e0d35607d530cd2dd5a5abb810fe76771
Tim Weber <scy@scytale.name>
2013-01-23 23:27:18 +0100

    Allow specifying a separate archival directory

--- run-autosnap-maintenance.sh
+++ run-autosnap-maintenance.sh
@@ -2,10 +2,11 @@
 
 # This is the main script to run. Pass the directory it should run in as first parameter.
 
-if [ "$#" -ne 1 ]; then
-	echo 'usage: run-autosnap-maintenance.sh DIRECTORY' >&2
-	echo '  DIRECTORY is the base autosnap directory where incoming/, 2012/ etc reside.' >&2
-	echo '  Please note that currently, DIRECTORY should really be an absolute path.' >&2
+if [ "$#" -lt 1 -o "$#" -gt 2 ]; then
+	echo 'usage: run-autosnap-maintenance.sh DIRECTORY [ARCHDIR]' >&2
+	echo '  DIRECTORY is the base autosnap directory where incoming/, today/ etc reside.' >&2
+	echo '  ARCHDIR is the directory where 2012/ etc reside. If omitted, defaults to DIRECTORY.' >&2
+	echo '  Please note that currently, directories should really be absolute paths.' >&2
 	exit 1
 fi
 
@@ -16,6 +17,15 @@ if [ ! -d "$AUTOSNAPDIR" ]; then
 	exit 2
 fi
 
+export ARCHDIR="$AUTOSNAPDIR"
+if [ "$#" -ge 2 ]; then
+	export ARCHDIR="$2"
+	if [ ! -d "$ARCHDIR" ]; then
+		echo "$ARCHDIR is not a directory" >&2
+		exit 2
+	fi
+fi
+
 # Change to the directory we're running from.
 cd "$(dirname "$0")"
 
@@ -23,7 +33,7 @@ cd "$(dirname "$0")"
 AUTOSNAPRNIN="$AUTOSNAPDIR/incoming" ./autosnap-renamer.sh
 
 # Start the mover.
-AUTOSNAPMVFROM="$AUTOSNAPDIR/incoming" AUTOSNAPMVTO="$AUTOSNAPDIR" ./autosnap-mover.sh
+AUTOSNAPMVFROM="$AUTOSNAPDIR/incoming" AUTOSNAPMVTO="$ARCHDIR" ./autosnap-mover.sh
 
 # Start the rsyncer.
-AUTOSNAPSYNCFROM="$AUTOSNAPDIR" AUTOSNAPSYNCTO="$AUTOSNAPDIR/today" ./autosnap-rsyncer.sh
+AUTOSNAPSYNCFROM="$ARCHDIR" AUTOSNAPSYNCTO="$AUTOSNAPDIR/today" ./autosnap-rsyncer.sh
