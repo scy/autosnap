9af0b2a16407fdab833656c6cad6678f43ad42af
Tim Weber <scy@scytale.name>
2013-01-27 23:30:47 +0100

    oipd13: Add script to generate HTML

---
+++ oipd13/build.sh
@@ -0,0 +1,52 @@
+#!/bin/sh -e
+
+OIPD13DIR="$(dirname "$0")"
+TODAYDIR="$1"
+
+cd "$OIPD13DIR"
+
+# Which file was the current one last time we ran?
+[ -f lastfile.txt ] && lastfile="$(cat lastfile.txt)"
+[ -f "$lastfile" ] || lastfile=''
+
+# Which file is currently the most recent one?
+currentfile="$(basename "$(find "$TODAYDIR" -iname '*autosnap*.jpg' | sort | tail -n 1)")"
+
+# If these are the same files, don't do anything.
+[ "$lastfile" = "$currentfile" ] && exit 0
+
+# Extract date and host out of the file name.
+filedatetime="$(echo "$currentfile" | sed -e 's/^\(....-..-..\)_\(..\)-\(..\)-\(..\)_autosnap_.*$/\1 \2:\3:\4/')"
+filehost="$(echo "$currentfile" | sed -e 's/^.*_autosnap_\([^.]*\).*$/\1/')"
+
+# Generate a device name out of the host name.
+case "$filehost" in
+	crys)
+		filedevice='Laptop'
+		;;
+	leafnode)
+		filedevice='Telefon'
+		;;
+	viewport)
+		filedevice='Tablet'
+		;;
+	*)
+		filedevice="„$filehost“"
+		;;
+esac
+
+# Copy the file over.
+cp "$TODAYDIR/$currentfile" .
+
+# Generate a new index.html.
+sed \
+	-e "/ id=\"photo\" /s/ src=\"[^\"]*\" / src=\"$currentfile\" /g" \
+	-e "/<figcaption>/s/YYYY-MM-DD hh:mm:ss/$filedatetime/g" \
+	-e "/<figcaption>/s/DEVICE/$filedevice/g" \
+	oipd13.html > index.html
+
+# Store last file.
+echo "$currentfile" > lastfile.txt
+
+# rsync all that stuff over.
+rsync -rtl ./ scy@eridanus.uberspace.de:www/scytale.name/proj/oipd13
